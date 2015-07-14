242d96e267c3f8a0ec3b3f2e1cea8138260e9e2c
MARGARET MORRIS <margaretblue@gmail.com>
2015-07-14 06:57:11 -0700

    generating random pics

--- random_art.py
+++ random_art.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
@@ -6,10 +7,33 @@ import random
 
 
 def create_expression():
+    cos = math.cos
+    sin = math.sin
+    tan = math.tan
+    pi = math.pi
+
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    expr = lambda x, y: (random.random() * 2) - 1
+    # expr = lambda x, y: (random.random() * (cos(x) ** 2) * x + (sin(x) **2) * x )
+    # expr = lambda x, y: (random.uniform(cos(x), sin(y) * sin(sin(pi * sin(pi * (sin(pi * sin(pi * sin(pi * cos(pi * y)))))
+
+    # expr = lambda x, y: (random.random() * 2) - 1
+    #expr = lambda x, y: (random.random() * cos(x)) - 1
+    expr = lambda x, y: (random.uniform(cos(x), sin(y) * sin(sin(pi * sin(pi * (sin(pi * sin(pi * sin(pi * cos(pi * y))))) * cos(pi * sin(pi * cos(pi * (pi * y) * (x * x)))))))))
+
+
     return expr
+
+def picker():
+    cos = math.cos
+    sin = math.sin
+    tan = math.tan
+    pi = math.pi
+    expressions = [
+        lambda x, y: (random.uniform(cos(x), sin(y) * sin(sin(pi * sin(pi * (sin(pi * sin(pi * sin(pi * cos(pi * y))))) * cos(pi * sin(pi * cos(pi * (pi * y) * (x * x)))))))))
+    ]
+    return expressions
+
 
 
 def run_expression(expr, x, y):
